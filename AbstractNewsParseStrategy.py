ca33d37331e9268c468354cb62ecffebfdfeb8da
balicanta <balicanta.yao@gmail.com>
2014-10-25 10:39:03 +0800

    Modify Comment and add publish date function

--- AbstractNewsParseStrategy.py
+++ AbstractNewsParseStrategy.py
@@ -3,7 +3,7 @@
 # @Author: Balicanta.Yao
 # @Date:   2014-10-24 13:34:05
 # @Last Modified by:   balicanta
-# @Last Modified time: 2014-10-25 00:10:19
+# @Last Modified time: 2014-10-25 10:31:24
  
 from abc import ABCMeta, abstractmethod
 
@@ -11,26 +11,23 @@ class AbstractNewsParseStrategy():
  
 	__metaclass__ = ABCMeta
  
-	# 檢驗傳入的 URL 是否符合用此 Strategy
+	# Check Url is matching this strategy
 	@abstractmethod
 	def isURLMatch(self, url):
 		pass
  
-	# 取得新聞的標題
+	# Get News Title
 	@abstractmethod
-	def getTitle(self, html_content):
+	def getTitle(self, beautiful_soup_object):
 		pass
  
-	# 取得記者名稱
+	# Get News Title
 	@abstractmethod
-	def getAuthor(self, html_content):
+	def getAuthor(self, beautiful_soup_object):
 		pass
- 
-	@abstractmethod
-	def getAuthor(self, html_content):
-		pass
- 
-	def getPublishDate(self, html_content):
+
+	# Get Publish Date
+	def getPublishDate(self, beautiful_soup_object):
 		pass
  
  
