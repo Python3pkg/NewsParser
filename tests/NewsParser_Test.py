3bd383a15902d8367097a4348de64c929732767b
balicanta <balicanta.yao@gmail.com>
2014-10-27 23:45:40 +0800

    Fix Test

--- tests/NewsParser_Test.py
+++ tests/NewsParser_Test.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 # @Author: balicanta
 # @Date:   2014-10-25 09:57:26
-# @Last Modified by:   bustta
-# @Last Modified time: 2014-10-27 23:22:08
+# @Last Modified by:   balicanta
+# @Last Modified time: 2014-10-27 23:44:57
 
 from NewsParser import NewsParser
 from requests.utils import get_encodings_from_content
 
 test_fixtures = [
     {"url": "http://udn.com/NEWS/NATIONAL/NAT3/9017464.shtml",
-        "title": "聯合報直擊", "author": "呂思逸"},
+        "title": "聯合報直擊", "author": "呂思逸","content":"是由陳老闆批了棉花棒"},
     {"url": "http://world.yam.com/post.php?id=2732",
-        "title": "海潮人潮兇", "content": "這座遊人如織的水都"},
+        "title": "海潮人潮兇", "author":"", "content": "這座遊人如織的水都"},
     {"url": "http://news.ltn.com.tw/news/business/breakingnews/1142153",
-        "title": "魏家退出101", "content": "財政部次長吳當傑今天傍晚表示"}
+        "title": "魏家退出101", "author":"", "content": "財政部次長吳當傑今天傍晚表示"}
 ]
 
 
