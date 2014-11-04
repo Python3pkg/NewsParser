03b93c6163c191b5feec4ffde9ac884bcbf7cbca
bustta <bustta80980@gmail.com>
2014-11-04 18:08:09 +0800

    rm encode_utf8

--- strategies/AnntwNewsParseStrategy.py
+++ strategies/AnntwNewsParseStrategy.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: bustta
 # @Date:   2014-11-02 12:43:04
-# @Last Modified by:   bustta
-# @Last Modified time: 2014-11-02 12:46:38
+# @Last Modified by:   kenny.tsai
+# @Last Modified time: 2014-11-04 17:50:55
 from AbstractNewsParseStrategy import AbstractNewsParseStrategy
 
 
@@ -15,19 +15,18 @@ class AnntwNewsParseStrategy(AbstractNewsParseStrategy):
 
     def getTitle(self, beautiful_soup_object):
         title = beautiful_soup_object.find(
-            "div", "content-pad").h3.text.encode("utf-8").strip()
+            "div", "content-pad").h3.text.strip()
         return title
 
     def getAuthor(self, beautiful_soup_object):
         author = beautiful_soup_object.find(
-            "div", "meta-block").a.text.encode("utf-8")
+            "div", "meta-block").a.text
         return author
 
     def getContent(self, beautiful_soup_object):
         content_div = beautiful_soup_object.find(
             "div", "markdown-body").find_all("p")
-        content = "\n".join(para.text.encode(
-            "utf-8").strip() for para in content_div)
+        content = "\n".join(para.text.strip() for para in content_div)
 
         return content
 
