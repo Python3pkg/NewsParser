7b8ba30efc8853ab19dfd95a97f56c8329ddfb4b
balicanta <balicanta.yao@gmail.com>
2014-11-05 22:23:10 +0800

    Add initi for beautiful API

--- newsParser/__init__.py
+++ newsParser/__init__.py
@@ -3,4 +3,6 @@
 # @Author: balicanta
 # @Date:   2014-11-01 21:02:00
 # @Last Modified by:   balicanta
-# @Last Modified time: 2014-11-01 21:02:00
+# @Last Modified time: 2014-11-05 22:19:01
+
+from NewsParser import NewsParser
