abae301cd48d16a6bb1dcd86f657439bcf981413
DinoLai <dinos80152@gmail.com>
2014-11-09 03:50:41 +0800

    add __all__ in init for import all

--- newsParser/strategies/__init__.py
+++ newsParser/strategies/__init__.py
@@ -2,6 +2,15 @@
 # -*- coding: utf-8 -*-
 # @Author: balicanta
 # @Date:   2014-11-01 21:01:48
-# @Last Modified by:   balicanta
-# @Last Modified time: 2014-11-01 21:06:39
+# @Last Modified by:   DinoLai
+# @Last Modified time: 2014-11-09 03:42:53
 
+__all__ = [
+    "AnntwNewsParseStrategy",
+    "CoolLoudParseStrategy",
+    "LtnNewsParseStrategy",
+    "PeopleNewsParseStrategy",
+    "StormMediaGroupParseStrategy",
+    "UdnNewsParseStrategy",
+    "WorldYamParseStrategy"
+    ]
