d6d7518c7c8fd6fd58a4ef8ae47876c7b8956cd4
balicanta <balicanta.yao@gmail.com>
2014-11-08 16:58:52 +0800

    加上 Version

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 # @Author: bustta
 # @Date:   2014-11-04 00:47:53
 # @Last Modified by:   balicanta
-# @Last Modified time: 2014-11-08 16:49:00
+# @Last Modified time: 2014-11-08 16:50:34
 
 from distutils.core import setup
 
 setup(
     name = 'newsParser',
+    version = newsParser.__version__,
     packages = ['newsParser', 'newsParser.strategies'],
     description = 'Taiwan News Parser',
     author = 'lab317',
